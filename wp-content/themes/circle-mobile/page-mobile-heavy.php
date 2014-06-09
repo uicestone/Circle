e3f0ed22d803338b539f63a050fe4ca68cf0288e
Uice Lu <uicestone@gmail.com>
2014-06-09 22:50:00 +0800

    删除多余的模板文件

--- wp-content/themes/circle-mobile/page-mobile-heavy.php
+++
@@ -1,8 +0,0 @@
-<?php
-get_header('mobile');
-?>
-<div class="head">
-	<img src="<?= get_template_directory_uri() ?>/img/mobile/logo.png" class="logo">
-</div>
-<?php wp_nav_menu(array('menu'=>'mobile-foot', 'container'=>false, 'menu_class'=>'nav')); ?>
-<?php get_footer('mobile'); ?>
