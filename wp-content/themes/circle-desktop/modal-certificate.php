56fb5e5ea8b6aa004f275ebe64ace137b4c38900
supersheep <supersheepxjy@gmail.com>
2014-06-12 22:00:52 +0800

    查看证书 fixed #4

---
+++ wp-content/themes/circle-desktop/modal-certificate.php
@@ -0,0 +1,13 @@
+<div id="modal-certificate" tabindex="-1" role="dialog" aria-labelledby="modal-certificate-label" aria-hidden="true" class="modal fade">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" data-dismiss="modal" aria-hidden="true" class="close">×</button>
+        <h4 id="modal-login-label" class="modal-title"></h4>
+      </div>
+      <div class="modal-body">
+        <img src="<?=get_template_directory_uri()?>/img/certification.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</div>
