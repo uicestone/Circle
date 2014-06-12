1711ba7170e36519b48a439ce08f45a6047e5284
supersheep <supersheepxjy@gmail.com>
2014-06-12 21:54:16 +0800

    品牌服务 fixed #3

---
+++ wp-content/themes/circle-desktop/modal-brand-service.php
@@ -0,0 +1,29 @@
+<div id="modal-brand-service" tabindex="-1" role="dialog" aria-labelledby="modal-brand-service-label" aria-hidden="true" class="modal fade">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" data-dismiss="modal" aria-hidden="true" class="close">×</button>
+        <h4 id="modal-login-label" class="modal-title"></h4>
+      </div>
+      <div class="modal-body">
+        <h2>珠宝服务（珠宝保养、珠宝维修、戒指尺寸调整服务和雕刻服务）</h2>
+
+        <h3>珠宝保养</h3>
+        <p>CIRCLE的彩色宝石是因其原石均来自纯正产地的品质至臻之作，通常需要每年进行一定的清洁。我们团队的工作人员拥有丰富的珠宝护理专业技能和实践经验，从宝石的清洁、抛光到养护无一不精。单价在3000元以上的珠宝，CIRCLE承诺2年内提供每年清洗一次的免费服务。</p>
+
+        <h3>珠宝维修</h3>
+        <p>CIRCLE缘点彩宝诚邀您将CIRCLE珠宝交于您的购货本店进行维修。对于凡在本店售出的珠宝，如有损坏，在珠宝售出一年内减免人工费，仅收取材料费（包括额外补金、配石、添加配件等）；售出一年以上的珠宝，客户需支付人工费和材料费。</p>
+
+        <h3>戒指尺寸调整服务</h3>
+        <p>确定戒指的精确尺寸是一项相对精细的工作，我们诚邀您参照CIRCLE戒指购买页面的尺寸指引选择适合自己的戒指尺寸。CIRCLE为您提供了7-15的戒圈尺寸可供您选择。</br>
+        若您对于在CIRCLE所购买的戒指需要尺寸上的调整，可在购物后七天内与销售凭证一并交回购货本店申请退换货，CIRCLE仅提供同款商品的7-15不同戒圈尺寸的退换。鉴于CIRCLE所出售的珠宝原材料及生产均来自海外，换货所需时间通常会视产品有无现货而定。
+        </p>
+
+        <h3>雕刻服务</h3>
+        <p>部分CIRCLE缘点彩宝可以通过雕刻进行个性化定制，内容可以是一个日期、首字母简称或者一个名字。</p>
+        <p>目前该项服务仅针对高级珠宝定制系列的商品开放，雕刻的可行性取决于珠宝的款式以及您希望雕刻的样式。您可将所需雕刻的样式进行备注，由CIRCLE缘点彩宝的团队进行可行性评估和操作执行。</p>
+
+      </div>
+    </div>
+  </div>
+</div>
