1758ccb1050f9cf08f5e1bd9363171e70a65ebbb
Uice Lu <uicestone@gmail.com>
2014-06-15 21:32:13 +0800

    fix: 微信二维码申请错误

--- wp-content/themes/circle-desktop/page-qrcode.php
+++ wp-content/themes/circle-desktop/page-qrcode.php
@@ -3,4 +3,4 @@
  * 申请一个微信带参数二维码，返回链接和过期时间
  */
 $wx = new WeixinAPI();
-echo json_encode($wx->generate_qr_code(array('action'=>$_GET['action'])));
+echo json_encode($wx->generate_qr_code(1,array('action'=>$_GET['action'])));
