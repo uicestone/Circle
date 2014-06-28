ca311c43f937d5665c5f14bd026d335e5ef2ca30
supersheep <supersheepxjy@gmail.com>
2014-06-28 21:22:33 +0800

    首页右下角“微信”“微博”链接进不去 #22

--- wp-content/themes/circle-desktop/footer.php
+++ wp-content/themes/circle-desktop/footer.php
@@ -4,12 +4,13 @@
 				<span class="menu-item"><span class="copy">&copy; 2014 circle</span></span>
 				<?php wp_nav_menu(array('theme_location'=>'desktop-foot', 'menu_class'=>'nav', 'container'=>false)); ?>
 				<div class="social"><span>与我们互动</span>
-					<img src="<?=get_template_directory_uri()?>/img/weixin.png">
-					<img src="<?=get_template_directory_uri()?>/img/weibo.png">
+					<a href="#" data-toggle="modal" data-target="#modal-wechat"><img src="<?=get_template_directory_uri()?>/img/weixin.png"></a>
+					<a href="http://weibo.com/circle9319" target="_blank"><img src="<?=get_template_directory_uri()?>/img/weibo.png"></a>
 				</div>
 			</div>
 		</div>
-		<?php get_template_part('modal','login') ?>
+    <?php get_template_part('modal','login') ?>
+		<?php get_template_part('modal','wechat') ?>
 		<?php get_template_part('modal','mine') ?>
 		<script>
 			// 弹层tab切换
---
+++ wp-content/themes/circle-desktop/modal-wechat.php
@@ -0,0 +1,13 @@
+<div id="modal-wechat" tabindex="-1" role="dialog" aria-labelledby="modal-login-wechat" aria-hidden="true" class="modal fade">
+    <div class="modal-dialog modal-md">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" data-dismiss="modal" aria-hidden="true" class="close">×</button>
+          <h4 id="modal-wechat-label" class="modal-title"></h4>
+        </div>
+        <div class="modal-body">
+          <img style="display:block;margin:0 auto;" src="<?=get_template_directory_uri()?>/img/wechat-qr.jpg" alt="">
+        </div>
+      </div>
+    </div>
+</div>
