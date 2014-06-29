ca5728e78a7b2ca884722ed60ed6bb05af282598
Uice Lu <uicestone@gmail.com>
2014-06-29 14:07:02 +0800

    移动版列表顶部图

--- wp-content/themes/circle-mobile/taxonomy-product_cat.php
+++ wp-content/themes/circle-mobile/taxonomy-product_cat.php
@@ -1,7 +1,8 @@
 <?php
-
 add_action('wp_enqueue_scripts', function(){
 	wp_enqueue_style('collection');
+	wp_enqueue_style('swiper');
+	wp_enqueue_script('swiper');
 });
 add_filter('body_class', function($class){
 	$class[] = 'nohead';
@@ -10,6 +11,27 @@ add_filter('body_class', function($class){
 get_header();
 ?>
 <div class="content">
+	<div class="slide">
+		<div class="swiper-container">
+			<div class="swiper-wrapper">
+				<?php
+				$category_intro = get_posts(array('name'=>get_queried_object()->slug))[0];
+				$gallery = get_post_meta($category_intro->ID, '_product_image_gallery', true);
+				$image_ids = $gallery ? explode(',', $gallery) : array();
+				foreach($image_ids as $image_id):
+				?>
+				<div class="swiper-slide">
+					<img src="<?=wp_get_attachment_url($image_id)?>" />
+				</div>
+				<?php endforeach; ?>
+			</div>
+		</div>
+		<div class="pagination"></div>
+		<div class="arrows">
+			<img src="<?=get_template_directory_uri()?>/img/arrow-left.png" class="prev" />
+			<img src="<?=get_template_directory_uri()?>/img/arrow-right.png" class="next" />
+		</div>
+	</div>
 	<ul class="list">
 		<?php while(have_posts()): the_post(); ?>
 		<li class="item">
@@ -24,4 +46,21 @@ get_header();
 		<?php endwhile; ?>
 	</ul>
 </div>
+<script>
+jQuery(function($){
+	var swiper = new Swiper('.swiper-container', {
+		pagination: '.pagination',
+		loop: true
+	});
+
+
+	$(".prev").on("click", function() {
+		swiper.swipePrev();
+	});
+
+	$(".next").on("click", function() {
+		swiper.swipeNext();
+	});
+});
+</script>
 <?php get_footer(); ?>
