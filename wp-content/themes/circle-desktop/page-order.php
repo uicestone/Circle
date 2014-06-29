38b3a6504a960556dde6ca39f0058f28176c1e3c
Uice Lu <uicestone@gmail.com>
2014-06-29 22:07:52 +0800

    修复: 订单API状态错误

--- wp-content/themes/circle-desktop/page-order.php
+++ wp-content/themes/circle-desktop/page-order.php
@@ -46,7 +46,7 @@ else{
 	foreach($posts as $post){
 		$order = array(
 			"date"=>$post->post_date,
-			"status"=>get_post_meta($id, 'status', true), // 订单状态
+			"status"=>get_post_meta($post->ID, 'status', true), // 订单状态
 			"province"=>get_post_meta($post->ID, 'province', true),//省市信息
 			"address"=>get_post_meta($post->ID, 'address', true),//详细地址
 			"zipcode"=>get_post_meta($post->ID, 'zipcode', true),
