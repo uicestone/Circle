89da4538c7fb9784af2e0e609e3b751ad14e7f4f
supersheep <supersheepxjy@gmail.com>
2014-07-04 23:27:40 +0800

    产品列表页，最上方的轮播箭头能不能去掉 fixes #29

--- wp-content/themes/circle-mobile/taxonomy-product_cat.php
+++ wp-content/themes/circle-mobile/taxonomy-product_cat.php
@@ -11,13 +11,16 @@ add_filter('body_class', function($class){
 get_header();
 ?>
 <div class="content">
+	<?php
+		$category_intro = get_posts(array('name'=>get_queried_object()->slug))[0];
+		$gallery = get_post_meta($category_intro->ID, '_product_image_gallery', true);
+		$image_ids = $gallery ? explode(',', $gallery) : array();
+	?>
+	<?php if(count($image_ids)): ?>
 	<div class="slide">
 		<div class="swiper-container">
 			<div class="swiper-wrapper">
 				<?php
-				$category_intro = get_posts(array('name'=>get_queried_object()->slug))[0];
-				$gallery = get_post_meta($category_intro->ID, '_product_image_gallery', true);
-				$image_ids = $gallery ? explode(',', $gallery) : array();
 				foreach($image_ids as $image_id):
 				?>
 				<div class="swiper-slide">
@@ -27,11 +30,14 @@ get_header();
 			</div>
 		</div>
 		<div class="pagination"></div>
+		<?php if(count($image_ids) > 1): ?>
 		<div class="arrows">
 			<img src="<?=get_template_directory_uri()?>/img/arrow-left.png" class="prev" />
 			<img src="<?=get_template_directory_uri()?>/img/arrow-right.png" class="next" />
 		</div>
+		<?php endif; ?>
 	</div>
+	<?php endif; ?>
 	<ul class="list">
 		<?php while(have_posts()): the_post(); ?>
 		<li class="item">
