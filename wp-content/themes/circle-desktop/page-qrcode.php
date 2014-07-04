e976f662360df9f5b623926499b99ede194acdcc
Uice Lu <uicestone@gmail.com>
2014-07-04 12:42:34 +0800

    设置二维码URL响应的过期时间 可能部分解决 #30

--- wp-content/themes/circle-desktop/page-qrcode.php
+++ wp-content/themes/circle-desktop/page-qrcode.php
@@ -3,4 +3,6 @@
  * 申请一个微信带参数二维码，返回链接和过期时间
  */
 $wx = new WeixinAPI();
+
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // 一个过去的时间，试图强制浏览器下次刷新
 echo json_encode($wx->generate_qr_code(1,array('action'=>$_GET['action'])));
