db3941816e31ae53a3f1aa113b46d6a030141098
supersheep <supersheepxjy@gmail.com>
2014-07-26 00:34:56 +0800

    add submenu for mobile fixes #49

--- wp-content/themes/circle-mobile/footer.php
+++ wp-content/themes/circle-mobile/footer.php
@@ -4,5 +4,26 @@
 			}
 		?>
 		<?php wp_footer(); ?>
+    <script>
+      $(".nav li").each(function(i,el){
+        var $el = $(el);
+        console.log($el,$el.find);
+        var submenu = $el.find(".sub-menu");
+        if(submenu.length){
+          $el.on("touchend", function(e){
+            e.stopPropagation();
+            if(submenu.css("display") == "none"){
+              submenu.show();
+            }else{
+              submenu.hide();
+            }
+          });
+        }
+      });
+
+      $("body").on("touchend",function(){
+        $(".sub-menu").hide();
+      });
+    </script>
 	</body>
 </html>
