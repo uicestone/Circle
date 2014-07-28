a30d5d033a53579d590ab9ff1153692902fde9f4
Uice Lu <uicestone@gmail.com>
2014-07-28 16:21:57 +0800

    更换首页和品牌页图片

--- wp-content/themes/circle-desktop/page-refresh-menu.php
+++ wp-content/themes/circle-desktop/page-refresh-menu.php
@@ -9,21 +9,21 @@ $wx->remove_menu();
 $data = array(
 	'button'=>array(
 		array(
-			'name'=>'彩宝精选',
+			'name'=>'购物专区',
 			'sub_button'=>array(
 				array(
 					'type'=>'view',
-					'name'=>'爱，从自己开始',
+					'name'=>'爱自己系列',
 					'url'=>'http://www.circlewava.com/product_series/%E7%88%B1%EF%BC%8C%E4%BB%8E%E8%87%AA%E5%B7%B1%E5%BC%80%E5%A7%8B/',
 				),
 				array(
 					'type'=>'view',
-					'name'=>'闺蜜的果香',
+					'name'=>'闺蜜果香系列',
 					'url'=>'http://www.circlewava.com/product_series/%E9%97%BA%E8%9C%9C%E7%9A%84%E6%9E%9C%E9%A6%99/'
 				),
 				array(
 					'type'=>'view',
-					'name'=>'高级彩宝',
+					'name'=>'高级彩宝系列',
 					'url'=>'http://www.circlewava.com/product_series/%E9%AB%98%E7%BA%A7%E5%BD%A9%E5%AE%9D%E5%AE%9A%E5%88%B6/'
 				),
 			)
@@ -35,6 +35,9 @@ $data = array(
 					'name'=>'品牌故事',
 					'type'=>'view',
 					'url'=>'http://www.circlewava.com/brand/'
+				),
+				array(
+					'name'=>'七夕活动'
 				),
 			)
 		),
